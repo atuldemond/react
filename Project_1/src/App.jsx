61ef7887f79c77003058d420e9007d371bc43c20
atuldemond <atuldemond@gmail.com>
2024-04-22 13:28:33 +0530

    day_2 of creating project-1

--- Project_1/src/App.jsx
+++ Project_1/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
         <nav className='w-[15%] h-full bg-zinc-100 pt-5 '>
 
-            <a href="/Create" className=' px-8  py-3  m-4  rounded-lg  border-4 border-green-200 text-blue-200  '>
+            <a href="/Create" className=' px-8  py-3  m-4  rounded-lg  border-4 border-green-300 text-black  '>
 
 
             Add New Product
@@ -21,13 +21,31 @@ function App() {
             </a>
 
           <hr />
-          <h1 className='ml-10  mt-4  font-bold  '>Category Filter</h1>
+          <h1 className='ml-10 mb-3 mt-8  font-bold  '>Category Filter</h1>
 
-          <ul className='bg-red-200 w-[50%] flex flex-col  justify-center items-center'>
-            <li className='bg-red-200 mb-2 '>
-              <span>cat 1</span>
+          <ul className=' w-[50%] flex flex-col  justify-center items-center'>
+            <li className=' mb-2  mt-2  flex justify-center items-center '>
+           <span className=' rounded-full  mr-3 bg-blue-200 w-[15px] h-[15px] '></span>
+              cart 1
+            </li>
+
+            <li className=' mb-2  mt-2  flex justify-center items-center '>
+           <span className=' rounded-full  mr-3 bg-red-200 w-[15px] h-[15px] '></span>
+              cart 2
+            </li>
+
+            <li className=' mb-2  mt-2  flex justify-center items-center '>
+           <span className=' rounded-full  mr-3 bg-green-200 w-[15px] h-[15px] '></span>
+              cart 3
             </li>
             
+            
+            <li className=' mb-2  mt-2  flex justify-center items-center '>
+           <span className=' rounded-full  mr-3 bg-orange-200 w-[15px] h-[15px] '></span>
+              cart 4
+            </li>
+
+
           </ul>
 
 
