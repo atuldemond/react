c4c3f2f3c013084b5d6febaa7790e9473dd499b2
atuldemond <atuldemond@gmail.com>
2024-04-27 14:38:02 +0530

    just changing some...

--- Project_1/src/App.jsx
+++ Project_1/src/App.jsx
@@ -1,46 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './Components/Home'
-import { Route,Routes,Link, useLocation } from 'react-router-dom'
-import Details from './Components/Details'
-import Create from './Components/Create'
+
+import "./App.css";
+import Home from "./Components/Home";
+import { Route, Routes, Link, useLocation } from "react-router-dom";
+import Details from "./Components/Details";
+import Create from "./Components/Create";
 //np work is done on 23/4/2024
-function App() { 
+function App() {
   // const [count, setCount] = useState(0)
 
-  const{search, pathname} = useLocation();
+  const { search, pathname } = useLocation();
   return (
     <div className=" h-screen w-screen flex">
+      {(pathname != "/" || search.length > 0) && (
+        <Link to="/" className="text-red-300 absolute left-[17%] top-[3%]">
+          Home
+        </Link>
+      )}
 
-
-        {(pathname != "/" || search.length > 0 ) &&(
-
-      <Link to="/" className="text-red-300 absolute left-[17%] top-[3%]">
-      Home
-      </Link>
-
-        )}
-  
-
-    <Routes>
- 
-    <Route path='/' element={<Home/>}/>
-    <Route path='/create' element={<Create/>}/>
-    <Route path='/details/:id' element={<Details/>}/>
- 
-    </Routes>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/details/:id" element={<Details />} />
+      </Routes>
     </div>
-    
-    
-
   );
-  
-  
-      
- 
-};
+}
 
-export default App
+export default App;
